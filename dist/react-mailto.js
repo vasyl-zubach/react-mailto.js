e33b41d16b228222e54a5aa5337a4108ed539181
Sebastian Germesin <neogermi@gmail.com>
2017-10-11 21:21:24 +0200

    update to react-16

--- dist/react-mailto.js
+++ dist/react-mailto.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -28,22 +32,22 @@ var Mailto = function (_Component) {
     function Mailto() {
         _classCallCheck(this, Mailto);
 
-        return _possibleConstructorReturn(this, Object.getPrototypeOf(Mailto).apply(this, arguments));
+        return _possibleConstructorReturn(this, (Mailto.__proto__ || Object.getPrototypeOf(Mailto)).apply(this, arguments));
     }
 
     _createClass(Mailto, [{
         key: '_prepareLink',
         value: function _prepareLink(cfg) {
-            var _cfg$subject = cfg.subject;
-            var subject = _cfg$subject === undefined ? '' : _cfg$subject;
-            var _cfg$to = cfg.to;
-            var to = _cfg$to === undefined ? '' : _cfg$to;
-            var _cfg$body = cfg.body;
-            var body = _cfg$body === undefined ? '' : _cfg$body;
-            var _cfg$cc = cfg.cc;
-            var cc = _cfg$cc === undefined ? '' : _cfg$cc;
-            var _cfg$bcc = cfg.bcc;
-            var bcc = _cfg$bcc === undefined ? '' : _cfg$bcc;
+            var _cfg$subject = cfg.subject,
+                subject = _cfg$subject === undefined ? '' : _cfg$subject,
+                _cfg$to = cfg.to,
+                to = _cfg$to === undefined ? '' : _cfg$to,
+                _cfg$body = cfg.body,
+                body = _cfg$body === undefined ? '' : _cfg$body,
+                _cfg$cc = cfg.cc,
+                cc = _cfg$cc === undefined ? '' : _cfg$cc,
+                _cfg$bcc = cfg.bcc,
+                bcc = _cfg$bcc === undefined ? '' : _cfg$bcc;
 
             var link = [];
             if (cc) {
@@ -63,16 +67,15 @@ var Mailto = function (_Component) {
     }, {
         key: 'render',
         value: function render() {
-            var _props = this.props;
-            var to = _props.to;
-            var cc = _props.cc;
-            var bcc = _props.bcc;
-            var onClick = _props.onClick;
-            var subject = _props.subject;
-            var body = _props.body;
-            var secure = _props.secure;
-
-            var props = _objectWithoutProperties(_props, ['to', 'cc', 'bcc', 'onClick', 'subject', 'body', 'secure']);
+            var _props = this.props,
+                to = _props.to,
+                cc = _props.cc,
+                bcc = _props.bcc,
+                onClick = _props.onClick,
+                subject = _props.subject,
+                body = _props.body,
+                secure = _props.secure,
+                props = _objectWithoutProperties(_props, ['to', 'cc', 'bcc', 'onClick', 'subject', 'body', 'secure']);
 
             var link = this._prepareLink({ to: to, cc: cc, bcc: bcc, subject: subject, body: body });
             var mailto = 'mailto:' + link;
@@ -91,7 +94,7 @@ var Mailto = function (_Component) {
     }, {
         key: '_handleSecureClick',
         value: function _handleSecureClick(href) {
-            var onClick = arguments.length <= 1 || arguments[1] === undefined ? function (e) {} : arguments[1];
+            var onClick = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function (e) {};
 
             return function (e) {
                 onClick.call(null, e);
@@ -106,6 +109,7 @@ var Mailto = function (_Component) {
 
 exports.default = Mailto;
 
+
 Mailto.displayName = 'Mailto';
 
 Mailto.defaultProps = {
@@ -113,11 +117,11 @@ Mailto.defaultProps = {
 };
 
 Mailto.propTypes = {
-    to: _react.PropTypes.oneOfType([_react.PropTypes.string.isRequired, _react.PropTypes.array.isRequired]),
-    cc: _react.PropTypes.oneOfType([_react.PropTypes.string, _react.PropTypes.array]),
-    bcc: _react.PropTypes.oneOfType([_react.PropTypes.string, _react.PropTypes.array]),
-    subject: _react.PropTypes.string,
-    body: _react.PropTypes.string,
-    onClick: _react.PropTypes.func,
-    children: _react.PropTypes.oneOfType([_react.PropTypes.array, _react.PropTypes.object, _react.PropTypes.string])
+    to: _propTypes2.default.oneOfType([_propTypes2.default.string.isRequired, _propTypes2.default.array.isRequired]),
+    cc: _propTypes2.default.oneOfType([_propTypes2.default.string, _propTypes2.default.array]),
+    bcc: _propTypes2.default.oneOfType([_propTypes2.default.string, _propTypes2.default.array]),
+    subject: _propTypes2.default.string,
+    body: _propTypes2.default.string,
+    onClick: _propTypes2.default.func,
+    children: _propTypes2.default.oneOfType([_propTypes2.default.array, _propTypes2.default.object, _propTypes2.default.string])
 };
