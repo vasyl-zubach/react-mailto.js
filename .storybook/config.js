08b0ed544ddf79e57cd908f64bb8166fae0391ad
Vasyl Zubach <vzubach@atlassian.com>
2019-10-19 22:26:57 +1100

    updated babel consig to only build an ES module

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
 import { configure } from '@storybook/react';
 
 import '@storybook/addon-actions/register';
-import '@storybook/addon-console';
 
 configure(require.context('../stories', true, /\.story\.(j|t)sx?$/), module);
--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-  plugins: ['@babel/transform-react-jsx', '@babel/plugin-transform-runtime'],
+  plugins: [
+    '@babel/transform-react-jsx'
+    // '@babel/plugin-transform-runtime'
+  ],
   presets: [
     '@babel/react',
-    '@babel/typescript',
-    [
-      '@babel/env',
-      {
-        debug: false,
-        targets: {
-          browsers: 'last 2 versions, ie >= 11'
-        },
-        modules: 'commonjs'
-        // useBuiltIns: 'usage',
-        // corejs: '3.0.0'
-        // shippedProposals: true,
-      }
-    ]
+    '@babel/typescript'
+    // [
+    //   '@babel/env',
+    //   {
+    //     debug: false,
+    //     targets: {
+    //       browsers: 'last 2 versions, ie >= 11'
+    //     },
+    //     modules: 'commonjs'
+    //     // useBuiltIns: 'usage',
+    //     // corejs: '3.0.0'
+    //     // shippedProposals: true,
+    //   }
+    // ]
   ]
 };
