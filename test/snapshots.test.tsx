168ea7a4984df6113a204c12610d97ab47ed7063
Vasyl Zubach <i@tjrus.com>
2021-01-02 13:21:15 +1100

    replace snapshot test global `it` with `test`

--- test/snapshots.test.tsx
+++ test/snapshots.test.tsx
@@ -9,7 +9,7 @@ global.Date.now = () => 1234567890;
 Object.keys(Stories)
   .filter((i) => i !== 'default')
   .forEach((storyTitle) => {
-    it(`Story: ${storyTitle}`, () => {
+    test(`Story: ${storyTitle}`, () => {
       const Component = Stories[storyTitle];
       const tree = renderer.create(<Component />).toJSON();
       expect(tree).toMatchSnapshot();
